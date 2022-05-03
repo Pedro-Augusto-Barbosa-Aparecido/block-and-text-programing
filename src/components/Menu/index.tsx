8dba0590086db33697f7e2f77a2fbbb68f421bc2
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-05-02 22:50:31 -0300

    Add some buttons on menu-lateral

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,11 +2,17 @@ import React, { useState } from 'react';
 
 import { BsFillArrowLeftSquareFill } from "react-icons/bs";
 import { MdMenu } from "react-icons/md";
+import { VscNewFile, VscNewFolder } from "react-icons/vsc";
 
 import { Container } from './styles';
 
-const Menu: React.FC = () => {
-  const [shrink, setShrink] = useState<boolean>(false);
+export type MenuProps = {
+  fileAndFolders?: Array<string> // mudar para tipo de folder e arquivo
+
+};
+
+const Menu: React.FC<MenuProps> = ({ fileAndFolders = [] }) => {
+  const [shrink, setShrink] = useState<boolean>(true);
 
   return <Container shrink={shrink}>
       <div className='button-menu-control'>
@@ -14,7 +20,22 @@ const Menu: React.FC = () => {
             shrink ? <MdMenu className='hover' size={30} color="rgba(20, 20, 255, .7)" onClick={() => setShrink(state => !state)} /> : 
                 <BsFillArrowLeftSquareFill className='hover' size={30} color="rgba(20, 20, 255, .7)" onClick={() => setShrink(state => !state)} />
         }
+        <div className='button-file-control'>
+          <div>
+            <VscNewFile className='hover' size={20} />
+            <p>New File</p>
+          </div>
+          <div>
+            <VscNewFolder className='hover' size={20} />
+            <p>New Folder</p>
+          </div>
+        </div>
       </div>
+      <ul>
+        {
+          fileAndFolders.length === 0 ? <p>No File or Project selected</p> : fileAndFolders.map((item, key) => <p key={key}>{item}</p>)
+        }
+      </ul>
   </Container>;
 }
 
--- src/components/Menu/styles.tsx
+++ src/components/Menu/styles.tsx
@@ -13,12 +13,63 @@ export const Container = styled.div<MenuProps>`
     height: 100%;
     ${props => props.shrink ? css`width: 60px !important;` : css`width: 400px !important;`}
 
+    overflow: hidden;
+
     div.button-menu-control {
-        padding: 15px;
+        padding-left: 15px;
+        height: 50px;
 
         border-bottom: 1px solid #e5e5e5;
 
-        width: 100%;
+        width: 400px;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+    }
+
+    div.button-file-control {
+        display: flex;
+
+        justify-content: flex-end;
+        align-items: center;
+
+        width: 80%;
+        height: 100%;
+
+        padding-right: 0;
+
+        cursor: pointer;
+
+        div {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            height: 100%;
+            width: 150px;
+        
+            p {
+                margin: 0;
+                margin-left: 10px;
+
+            }
+
+        }
+
+        div:not(:last-child) {
+            justify-content: flex-end;
+
+        }
+
+        div:active {
+            p, svg {
+                transform: scale(.95);
+
+            }
+
+        }
 
     }
 
