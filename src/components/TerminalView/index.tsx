1623f1c2580fc6809c65026146e7c25ce0c9b97f
Pedro Augusto Barbosa Aparecido <pedroaugustobarosa@inatel.br>
2022-05-04 19:57:34 -0300

    Adding TerminalComponent on Project

---
+++ src/components/TerminalView/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { TerminalContainer } from './styles';
+
+const TerminalView: React.FC = () => {
+  return <TerminalContainer>
+
+  </TerminalContainer>;
+}
+
+export default TerminalView;
